8d1e6aab91d75b3728ee433f39d235f3c9fb9a5a
kazra <204182586+kazra2025@users.noreply.github.com>
2025-06-06 07:05:06 -0400

    init module

---
+++ src/kkutilz/__init__.py
@@ -0,0 +1,8 @@
+# kkutilz/__init__.py
+
+__version__ = "1.0.0"
+__author__ = "Kourosh Khosravi"
+__license__ = "MIT License"
+__status__ = "Development"
+__url__ = "https://github.com/kouritron/kkutilz"
+
